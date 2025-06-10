9392b17a8bb594eead03947eff7c96fe61d34a29
Usman Ali <127815926+u5man-ali@users.noreply.github.com>
2025-06-10 10:51:46 +0500

    light/dark mode implementation complete

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -27,39 +27,39 @@ const styles = {
     link: "bg-transparent text-brand-primary-default hover:underline active:text-brand-primary-focused"
   },
     secondary: {
-    gradient: "relative bg-gradient-to-b from-neutral-rest to-neutral-focus text-neutral-white hover:bg-gradient-to-b hover:from-neutral-500 hover:to-neutral-700 active:ring-1 active:ring-offset-1 active:ring-neutral-500 active:bg-gradient-to-b active:from-neutral-800 active:to-neutral-600 focus:bg-gradient-to-b focus:from-neutral-700 focus:to-neutral-900 align-middle select-none text-center duration-100 ease-in shadow-sm hover:shadow-md focus:shadow-none after:absolute after:inset-0 after:rounded-[inherit] after:shadow-[inset_0px_1px_1px_1px_rgba(255,255,255,0.35),inset_0_-1px_1px_1px_rgba(0,0,0,0.25)]",
+    gradient: "relative bg-gradient-to-b from-neutral-rest to-neutral-focus text-neutral-white hover:bg-gradient-to-b hover:from-neutral-hover hover:to-neutral-rest active:ring-1 active:ring-offset-1 active:ring-neutral-regular active:bg-gradient-to-b active:from-neutral-focus active:to-neutral-rest focus:bg-gradient-to-b focus:from-neutral-hover focus:to-neutral-focus align-middle select-none text-center duration-100 ease-in shadow-sm hover:shadow-md focus:shadow-none after:absolute after:inset-0 after:rounded-[inherit] after:shadow-[inset_0px_1px_1px_1px_rgba(255,255,255,0.35),inset_0_-1px_1px_1px_rgba(0,0,0,0.25)]",
     solid: "bg-neutral-rest hover:bg-neutral-hover text-neutral-white active:bg-neutral-focus ",
     outline: "border-2 border-neutral-rest text-neutral-default hover:border-neutral-hover hover:text-neutral-hovered active:bg-neutral-bg active:text-neutral-focused",
     transparent: "bg-transparent text-neutral-default hover:bg-neutral-secondary active:bg-neutral-bg",
     link: "bg-transparent text-neutral-default hover:underline active:text-neutral-focused"
   },
   success: {
-    gradient: "relative bg-gradient-to-b from-success-600 to-success-800 text-white-50 hover:bg-gradient-to-b hover:from-success-500 hover:to-success-700 active:ring-1 active:ring-offset-1 active:ring-success-500 active:bg-gradient-to-b active:from-success-800 active:to-success-600 focus:bg-gradient-to-b focus:from-success-700 focus:to-success-900 align-middle select-none text-center duration-100 ease-in shadow-sm hover:shadow-md after:absolute after:inset-0 after:rounded-[inherit] after:shadow-[inset_0px_1px_1px_1px_rgba(255,255,255,0.35),inset_0_-1px_1px_1px_rgba(0,0,0,0.25)]",
-    solid: "bg-success-700 hover:bg-success-600 text-white-50 active:bg-success-700 ",
-    outline: "border-2 border-success-700 text-success-600 hover:border-success-600 hover:text-success-600 active:bg-success-50 active:text-success-800",
-    transparent: "bg-transparent text-success-700 hover:bg-success-50 active:bg-success-100",
-    link: "bg-transparent text-success-700 hover:underline active:text-success-800"
+    gradient: "relative bg-gradient-to-b from-semantic-success-rest to-semantic-success-focus text-neutral-white hover:bg-gradient-to-b hover:from-semantic-success-hover hover:to-semantic-success-rest active:ring-1 active:ring-offset-1 active:ring-semantic-success-regular active:bg-gradient-to-b active:from-semantic-success-focus active:to-semantic-success-rest focus:bg-gradient-to-b focus:from-semantic-success-hover focus:to-semantic-success-focus align-middle select-none text-center duration-100 ease-in shadow-sm hover:shadow-md after:absolute after:inset-0 after:rounded-[inherit] after:shadow-[inset_0px_1px_1px_1px_rgba(255,255,255,0.35),inset_0_-1px_1px_1px_rgba(0,0,0,0.25)]",
+    solid: "bg-semantic-success-rest hover:bg-semantic-success-hover text-neutral-white active:bg-semantic-success-focus ",
+    outline: "border-2 border-semantic-success-rest text-semantic-success-default hover:border-semantic-success-hover hover:text-semantic-success-hovered active:bg-semantic-success-bg active:text-semantic-success-focused",
+    transparent: "bg-transparent text-semantic-success-default hover:bg-semantic-success-light active:bg-semantic-success-bg",
+    link: "bg-transparent text-semantic-success-default hover:underline active:text-semantic-success-focused"
   },
   error: {
-    gradient: "relative bg-gradient-to-b from-error-600 to-error-800 text-white-50 hover:bg-gradient-to-b hover:from-error-500 hover:to-error-700 active:ring-1 active:ring-offset-1 active:ring-error-500 active:bg-gradient-to-b active:from-error-800 active:to-error-600 focus:bg-gradient-to-b focus:from-error-700 focus:to-error-900 align-middle select-none text-center duration-100 ease-in shadow-sm hover:shadow-md after:absolute after:inset-0 after:rounded-[inherit] after:shadow-[inset_0px_1px_1px_1px_rgba(255,255,255,0.35),inset_0_-1px_1px_1px_rgba(0,0,0,0.25)]",
-    solid: "bg-error-700 hover:bg-error-600 text-white-50 active:bg-error-700 ",
-    outline: "border-2 border-error-700 text-error-600 hover:border-error-600 hover:text-error-600 active:bg-error-50 active:text-error-800",
-    transparent: "bg-transparent text-error-700 hover:bg-error-50 active:bg-error-100",
-    link: "bg-transparent text-error-700 hover:underline active:text-error-800"
+    gradient: "relative bg-gradient-to-b from-semantic-error-rest to-semantic-error-focus text-neutral-white hover:bg-gradient-to-b hover:from-semantic-error-hover hover:to-semantic-error-rest active:ring-1 active:ring-offset-1 active:ring-semantic-error-regular active:bg-gradient-to-b active:from-semantic-error-focus active:to-semantic-error-hover focus:bg-gradient-to-b focus:from-semantic-error-hover focus:to-semantic-error-focus align-middle select-none text-center duration-100 ease-in shadow-sm hover:shadow-md after:absolute after:inset-0 after:rounded-[inherit] after:shadow-[inset_0px_1px_1px_1px_rgba(255,255,255,0.35),inset_0_-1px_1px_1px_rgba(0,0,0,0.25)]",
+    solid: "bg-semantic-error-rest hover:bg-semantic-error-hover text-neutral-white active:bg-semantic-error-focus ",
+    outline: "border-2 border-semantic-error-rest text-semantic-error-default hover:border-semantic-error-hover hover:text-semantic-error-hovered active:bg-semantic-error-bg active:text-semantic-error-focused",
+    transparent: "bg-transparent text-semantic-error-default hover:bg-semantic-error-light active:bg-semantic-error-bg",
+    link: "bg-transparent text-semantic-error-default hover:underline active:text-semantic-error-focused"
   },
   warning: {
-    gradient: "relative bg-gradient-to-b from-warning-600 to-warning-800 text-white-50 hover:bg-gradient-to-b hover:from-warning-500 hover:to-warning-700 active:ring-1 active:ring-offset-1 active:ring-warning-500 active:bg-gradient-to-b active:from-warning-800 active:to-warning-600 focus:bg-gradient-to-b focus:from-warning-700 focus:to-warning-900 align-middle select-none text-center duration-100 ease-in shadow-sm hover:shadow-md after:absolute after:inset-0 after:rounded-[inherit] after:shadow-[inset_0px_1px_1px_1px_rgba(255,255,255,0.35),inset_0_-1px_1px_1px_rgba(0,0,0,0.25)]",
-    solid: "bg-warning-700 hover:bg-warning-600 text-white-50 active:bg-warning-700 ",
-    outline: "border-2 border-warning-700 text-warning-600 hover:border-warning-600 hover:text-warning-600 active:bg-warning-50 active:text-warning-800",
-    transparent: "bg-transparent text-warning-700 hover:bg-warning-50 active:bg-warning-100",
-    link: "bg-transparent text-warning-700 hover:underline active:text-warning-800"
+    gradient: "relative bg-gradient-to-b from-semantic-warning-rest to-semantic-warning-focus text-neutral-white hover:bg-gradient-to-b hover:from-semantic-warning-hover hover:to-semantic-warning-rest active:ring-1 active:ring-offset-1 active:ring-semantic-warning-regular active:bg-gradient-to-b active:from-semantic-warning-focus active:to-semantic-warning-hover focus:bg-gradient-to-b focus:from-semantic-warning-hover focus:to-warning-focus align-middle select-none text-center duration-100 ease-in shadow-sm hover:shadow-md after:absolute after:inset-0 after:rounded-[inherit] after:shadow-[inset_0px_1px_1px_1px_rgba(255,255,255,0.35),inset_0_-1px_1px_1px_rgba(0,0,0,0.25)]",
+    solid: "bg-semantic-warning-rest hover:bg-semantic-warning-hover text-nuetral-white active:bg-semantic-warning-focus ",
+    outline: "border-2 border-semantic-warning-rest text-semantic-warning-default hover:border-semantic-warning-hover hover:text-semantic-warning-hovered active:bg-semantic-warning-bg active:text-semantic-warning-focused",
+    transparent: "bg-transparent text-semantic-warning-default hover:bg-semantic-warning-light active:bg-semantic-warning-bg",
+    link: "bg-transparent text-semantic-warning-default hover:underline active:text-semantic-warning-focused"
   },
   info: {
-    gradient: "relative bg-gradient-to-b from-info-600 to-info-800 text-white-50 hover:bg-gradient-to-b hover:from-info-500 hover:to-info-700 active:ring-1 active:ring-offset-1 active:ring-info-500 active:bg-gradient-to-b active:from-info-800 active:to-info-600 focus:bg-gradient-to-b focus:from-info-700 focus:to-info-900 align-middle select-none text-center duration-100 ease-in shadow-sm hover:shadow-md after:absolute after:inset-0 after:rounded-[inherit] after:shadow-[inset_0px_1px_1px_1px_rgba(255,255,255,0.35),inset_0_-1px_1px_1px_rgba(0,0,0,0.25)]",
-    solid: "bg-info-700 hover:bg-info-600 text-white-50 active:bg-info-700 ",
-    outline: "border-2 border-info-700 text-info-600 hover:border-info-600 hover:text-info-600 active:bg-info-50 active:text-info-800",
-    transparent: "bg-transparent text-info-700 hover:bg-info-50 active:bg-info-100",
-    link: "bg-transparent text-info-700 hover:underline active:text-info-800"
+    gradient: "relative bg-gradient-to-b from-semantic-info-rest to-semantic-info-focus text-neutral-white hover:bg-gradient-to-b hover:from-semantic-info-hover hover:to-semantic-info-rest active:ring-1 active:ring-offset-1 active:ring-semantic-info-regular active:bg-gradient-to-b active:from-semantic-info-focus active:to-semantic-info-hover focus:bg-gradient-to-b focus:from-semantic-info-hover focus:to-semantic-info-focus align-middle select-none text-center duration-100 ease-in shadow-sm hover:shadow-md after:absolute after:inset-0 after:rounded-[inherit] after:shadow-[inset_0px_1px_1px_1px_rgba(255,255,255,0.35),inset_0_-1px_1px_1px_rgba(0,0,0,0.25)]",
+    solid: "bg-semantic-info-rest hover:bg-semantic-info-hover text-neutral-white active:bg-semantic-info-focus ",
+    outline: "border-2 border-semantic-info-rest text-semantic-info-default hover:border-semantic-info-hover hover:text-semantic-info-hovered active:bg-semantic-info-bg active:text-semantic-info-focused",
+    transparent: "bg-transparent text-semantic-info-default hover:bg-semantic-info-light active:bg-semantic-info-bg",
+    link: "bg-transparent text-semantic-info-default hover:underline active:text-semantic-info-focused"
   },
 };
 
